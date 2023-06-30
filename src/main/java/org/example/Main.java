85ecac1bf731db58817b7fa95375efcf60959853
kimdaeyoung <crossapp86@gmail.com>
2023-06-30 23:16:25 +0900

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,14 @@
 
 package org.example;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-    int add_all = 0;
-    for (int i = 0; i < 999; i++) {
-        if ((i + 1) % 3 == 0 || (i + 1) % 5 == 0) add_all += (i + 1);
-    }
-    System.out.println(add_all);
+        int sum = IntStream.range(1, 1000) // 1부터 999까지의 자연수 범위
+                .filter(n -> n % 3 == 0 || n % 5 == 0) // 3, 5로 나누어 떨어지는 자연수 필터링
+                .sum(); // 총합 계산
+// sum 메서드는 long 타입으로 반환되지만 이 문제에서는 범위를 초과할 수 없어 고려할 필요 없음.
+        System.out.println("총합: " + sum);
     }
 }
